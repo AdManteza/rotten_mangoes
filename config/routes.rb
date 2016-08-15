93c8ac2ad909b5a0be8294f649a4852df307dcc7
AdManteza <vagrant@lighthouse-labs>
2016-08-15 20:51:24 +0000

    completed exercise 9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+
+  # get 'sessions/create'
+
+  # get 'users/new'
+
+  # get 'users/create'
+
   # get 'movies/index'
 
   # get 'movies/show'
@@ -10,4 +18,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :movies
+
+  resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create]
 end
