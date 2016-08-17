6d1d860dfe205699ac0fa5ebe3c9dc209d743c8d
AdManteza <vagrant@lighthouse-labs>
2016-08-17 17:16:30 +0000

    FIxed the feature/admins

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
-  before_filter :check_admin
+  before_action :check_login
+  before_action :check_admin
   
   def index
     @users = User.all
